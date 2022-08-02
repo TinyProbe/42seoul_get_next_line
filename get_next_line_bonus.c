34e2ab5cf7b012ae487b96040d05dbc72bc224b4
TinyProbe <137adv167@gmail.com>
2022-08-02 17:03:36 +0900

    some test files

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,47 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 16:08:39 by tkong             #+#    #+#             */
+/*   Updated: 2022/08/02 16:26:15 by tkong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line_bonus.h"
 
-static t_buf    *get_buf(t_buf *buf, int fd);
-static ssize_t  fill_buf(t_buf *buf);
+static t_buf	*get_buf(t_buf *buf, int fd);
+static ssize_t	fill_buf(t_buf *buf);
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static t_buf    buf[MAX_FILE];
-    t_buf           *cur;
-    char            str[MAX_LENGTH];
-    int             idx;
+	static t_buf	buf[MAX_FILE];
+	t_buf			*cur;
+	char			str[MAX_LENGTH];
+	int				idx;
 
-    cur = buf;
-    if (fd)
-        cur = get_buf(buf + 1, fd);
-    idx = 0;
-    while (1)
-    {
-        if (cur->idx == BUFFER_SIZE)
-            if (fill_buf(cur) <= 0)
-            {
-                cur->idx = BUFFER_SIZE;
-                return (0);
-            }
-        str[idx++] = cur->buf[cur->idx++];
-        if (str[idx - 1] == '\n' || !~str[idx - 1] || !str[idx - 1])
-            return (ft_strdup(str, idx));
-    }
+	cur = buf;
+	if (fd)
+		cur = get_buf(buf + 1, fd);
+	idx = 0;
+	while (1)
+	{
+		if (cur->idx == BUFFER_SIZE && fill_buf(cur) <= 0)
+		{
+			cur->idx = BUFFER_SIZE;
+			return (0);
+		}
+		str[idx++] = cur->buf[cur->idx++];
+		if (str[idx - 1] == '\n' || !~str[idx - 1] || !str[idx - 1])
+			return (ft_strdup(str, idx));
+	}
 }
 
-static t_buf    *get_buf(t_buf *buf, int fd)
+static t_buf	*get_buf(t_buf *buf, int fd)
 {
-    while (buf->fd && buf->fd != fd)
-        ++buf;
-    if (!(buf->fd))
-    {
-        buf->fd = fd;
-        buf->idx = BUFFER_SIZE;
-    }
-    return (buf);
+	while (buf->fd && buf->fd != fd)
+		++buf;
+	if (!(buf->fd))
+	{
+		buf->fd = fd;
+		buf->idx = BUFFER_SIZE;
+	}
+	return (buf);
 }
 
-static ssize_t  fill_buf(t_buf *buf)
+static ssize_t	fill_buf(t_buf *buf)
 {
-    buf->idx = 0;
-    return (read(buf->fd, buf->buf, BUFFER_SIZE));
+	buf->idx = 0;
+	return (read(buf->fd, buf->buf, BUFFER_SIZE));
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 16:08:42 by tkong             #+#    #+#             */
+/*   Updated: 2022/08/02 16:08:43 by tkong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -5,20 +17,20 @@
 #  define BUFFER_SIZE 10
 # endif
 
-# define MAX_FILE   100000000 / BUFFER_SIZE
-# define MAX_LENGTH 100000
+# define MAX_FILE	100000000 / BUFFER_SIZE
+# define MAX_LENGTH	1000000
 
 # include <unistd.h>
 # include <stdlib.h>
 
 typedef struct s_buf
 {
-    char    buf[BUFFER_SIZE];
-    int     idx;
-    int     fd;
-}   t_buf;
+	char	buf[BUFFER_SIZE];
+	int		idx;
+	int		fd;
+}	t_buf;
 
-char    *get_next_line(int fd);
-char    *ft_strdup(const char *s1, size_t len);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1, size_t len);
 
 #endif
